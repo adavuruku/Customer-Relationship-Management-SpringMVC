2fbbd219744d3273c76ec253b045b1ed1b29855a
DESKTOP-J6P0TNG\SHERIFF <akinade.tumise@outsourceglobal.net>
2021-01-20 00:43:47 +0100

    In Progress

---
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,72 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository //is applied to only dao - @component - is class - @controller for controllers
+public class CustomerDAOImpl implements CustomerDAO {
+
+	// need to inject the session factory
+	//using autowired since factory is defined in spring-mvc-crud-demo-servlet.xml
+//	@Autowired
+//	private SessionFactory sessionFactory;
+	
+	// @Transactional //when this is added u dont need to start and end transaction manually
+	@Override
+	public List<Customer> getCustomers() {
+		
+		// get the current hibernate session
+//		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Session currentSession =  HibernateUtil.getSessionFactory().openSession();
+				
+		// create a query  ... sort by last name
+		Query<Customer> theQuery = 
+				currentSession.createQuery("from Customer order by lastName",
+											Customer.class);
+		
+		// execute query and get result list
+		List<Customer> customers = theQuery.getResultList();
+				
+		// return the results		
+		return customers;
+	}
+
+	@Override
+	public void saveCustomer(Customer theCustomer) {
+
+		// get current hibernate session
+//		Session currentSession = sessionFactory.getCurrentSession();
+		Session currentSession =  HibernateUtil.getSessionFactory().openSession();
+		// save/upate the customer ... finally LOL
+		currentSession.saveOrUpdate(theCustomer);
+		
+	}
+
+	@Override
+	public Customer getCustomer(int theId) {
+
+		// get the current hibernate session
+//		Session currentSession = sessionFactory.getCurrentSession();
+		Session currentSession =  HibernateUtil.getSessionFactory().openSession();
+		// now retrieve/read from database using the primary key
+		Customer theCustomer = currentSession.get(Customer.class, theId);
+		
+		return theCustomer;
+	}
+
+}
+
+
+
+
+
+
---
+++ src/com/luv2code/springdemo/entity/Customer.java
@@ -0,0 +1,86 @@
+package com.luv2code.springdemo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.dialect.pagination.FirstLimitHandler;
+
+@Entity
+@Table(name="customer")
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String emailAdd;
+	
+	
+	
+	public Customer() {
+		
+	}
+
+	public Customer(String firstName, String lastName, 
+			String emailAdd) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailAdd = emailAdd;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailAdd() {
+		return emailAdd;
+	}
+
+	public void setEmailAdd(String emailAdd) {
+		this.emailAdd = emailAdd;
+	}
+	
+	
+	    
+	@Override
+	public String toString() {
+		return "Student [Id = " +id +", FirstName = " + firstName
+				+ ", Last Name = " + lastName + ", Email = " + emailAdd + "]";
+	}
+} 
